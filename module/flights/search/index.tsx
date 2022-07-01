b8a8d309ad182dc274c0c198b01e56a0db717c83
unxoun <60313778+unxoun@users.noreply.github.com>
2022-07-01 10:56:06 -0800

    Create index.tsx

---
+++ module/flights/search/index.tsx
@@ -0,0 +1,13 @@
+// line 848
+              <FilterTicket
+                sections={[
+                  {
+                    id: 'price',
+                    title: 'قیمت',
+                    uiControl: 'slider',
+                    items: [
+                      {title: 'از', value: 1000},
+                      {title: 'تا', value: 2000},
+                    ],
+                  },
+                ]}
